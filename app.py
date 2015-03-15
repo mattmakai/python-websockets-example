2a07324a25545055c3ce7689710f8632b40950a0
Matt Makai <makaimc@gmail.com>
2015-03-15 16:41:22 -0400

    bumping to step 3

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
+from gevent import monkey
+monkey.patch_all()
+
 import redis
 from flask import Flask, render_template, request
+from flask.ext.socketio import SocketIO
 
 app = Flask(__name__)
 db = redis.StrictRedis('localhost', 6379, 0)
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def main():
-    c = db.incr('user_count')
-    return render_template('main.html', connected=c)
+    return render_template('main.html')
+
+
+@socketio.on('connect', namespace='/dd')
+def ws_conn():
+    c = db.incr('connected')
+    socketio.emit('msg', {'count': c}, namespace='/dd')
+
+
+@socketio.on('disconnect', namespace='/dd')
+def ws_disconn():
+    c = db.decr('connected')
+    socketio.emit('msg', {'count': c}, namespace='/dd')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
